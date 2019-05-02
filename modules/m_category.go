03f06b7b86d9423f1c9d1dd5d24ecdd056919661
liyiwen <liyiwen@huoyunren.com>
2019-05-02 11:50:44 +0800

    categories interface

--- modules/m_category.go
+++ modules/m_category.go
@@ -18,7 +18,7 @@ type CreateCategoryBody struct {
 	// 排序
 	Sort int32 `json:"sort" default:"0"`
 	// 是否保留为系统预设
-	Reserved enumeration.Bool `json:"reserved" default:"0"`
+	Reserved enumeration.Bool `json:"reserved" default:"UNKNOWN"`
 }
 
 func CreateCategory(req CreateCategoryBody, db *sqlx.DB) (result *database.Category, err error) {
@@ -47,7 +47,7 @@ type UpdateCategoryBody struct {
 	// 排序
 	Sort int32 `json:"sort" default:"0"`
 	// 是否保留为系统预设
-	Reserved enumeration.Bool `json:"reserved" default:"0"`
+	Reserved enumeration.Bool `json:"reserved" default:"UNKNOWN"`
 }
 
 func UpdateCategory(categoryKey string, req UpdateCategoryBody, db *sqlx.DB, withLock bool) error {
