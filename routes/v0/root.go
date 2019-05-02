49909b8858160a2c5d16c7cd8d61447469ae8d85
liyiwen <liyiwen@huoyunren.com>
2019-05-02 09:45:35 +0800

    categories interface

--- routes/v0/root.go
+++ routes/v0/root.go
@@ -2,6 +2,7 @@ package v0
 
 import (
 	"github.com/in2store/service-in2-book/routes/v0/books"
+	"github.com/in2store/service-in2-book/routes/v0/categories"
 	"github.com/in2store/service-in2-book/routes/v0/metas"
 	"github.com/johnnyeven/libtools/courier"
 )
@@ -11,6 +12,7 @@ var Router = courier.NewRouter(V0Group{})
 func init() {
 	Router.Register(books.Router)
 	Router.Register(metas.Router)
+	Router.Register(categories.Router)
 }
 
 type V0Group struct {
